66a0b923bf99710ddee6842f78b8e5dacea1baa6
Sidhra <sidhra.cader@hotmail.com>
2017-08-31 09:33:49 +1000

    completed startup generator

--- day3/index.js
+++ day3/index.js
@@ -15,10 +15,18 @@ for (var num = 1; num < 200; num++) {
 console.log("\n \n");
 
 console.log("Every second odd number between 1 and 200");
-for (var num2 = 1; num2 < 200; num2 += 4) {
+var skip = false;
+for (var num2 = 3; num2 < 200; num2 += 4) {
+    // if (num2 % 2 === 1) {
+    //   console.log(num2);
+    // }
     if (num2 % 2 === 1) {
+      if (!skip) {
+        skip = true;
+      }
       console.log(num2);
     }
+    skip = false;
 }
 console.log("\n \n");
 
